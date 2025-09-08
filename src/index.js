39c01ed4424510975cfbde62258aeb6a8dea8fd5
hanankhan98 <hanankhan9191@gmail.com>
2025-09-08 12:15:49 +0500

    Comp ALl Changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 // Root endpoint
 app.get("/", (req, res) => {
-  res.send("hello from the NEW deployment version 2!");
+  res.send("hello from the NEW deployment version 2! happy to add the new chnages hanan khan");
 });
 
 app.listen(3000, "0.0.0.0", () => {
